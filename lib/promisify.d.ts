d3defac25c2c37df1406516a0954f71b72d282d0
Eray Hanoğlu <erayszn@gmx.net>
2020-11-18 16:43:06 +0300

    Updated dependencies Updated typing

--- lib/promisify.d.ts
+++ lib/promisify.d.ts
@@ -6,7 +6,7 @@ declare function promisify(fn: () => any);
 
 declare namespace promisify {
 
-    export function fromCallback(cb: (error, value) => void);
+    export function fromCallback<T = any>(cb: (error, value: T) => void): Promise<T>;
 
     export function isPromise(x: any): boolean;
 
